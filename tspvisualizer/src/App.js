749f2d352440b54cf2bf12ea18c2d3d656d8819d
peter <k88kklo5418@gmail.com>
2022-10-06 16:45:07 +0800

    commit

---
+++ tspvisualizer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>My first React App</h1>
+          <h2>Author : Peter Chang</h2>
+
+    </div>
+  );
+}
+
+export default App;
