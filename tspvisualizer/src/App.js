0a28832797e28e679d5f38f7e23dea696b68ff63
peter <k88kklo5418@gmail.com>
2022-10-07 17:32:24 +0800

    commit

--- tspvisualizer/src/App.js
+++ tspvisualizer/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="App">
           <h1>My first React App</h1>
           <h2>Author : Peter Chang</h2>
+          <Button variant="contained">Hello World</Button>
+          
+          
 
     </div>
   );
